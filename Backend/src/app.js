70a530cb81cbab385d8e674e441f02a64bc74a18
Kunal Tailor <kunaltailor5555@gmail.com>
2026-04-17 11:36:04 +0530

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+/* require all the routes here */
+const authRouter = require("./routes/auth.routes")
+const interviewRouter = require("./routes/interview.routes")
+
+
+/* using all the routes here */
+app.use("/api/auth", authRouter)
+app.use("/api/interview", interviewRouter)
+
+
+/* global error handler */
+app.use((err, req, res, next) => {
+    console.error("ERROR:", err.message)
+    console.error(err.stack)
+    res.status(500).json({
+        message: "Internal server error",
+        error: err.message
+    })
+})
+
+module.exports = app
